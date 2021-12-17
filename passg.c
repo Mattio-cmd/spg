438e729ede3645f9fd2f80ee553a38ed9ba4cc18
Mattio-cmd <coghiqm8@gmail.com>
2021-12-16 19:19:41 -0600

    Removed the C version

--- passg.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main () {
-  char *lower = "qwertyuiopasdfghjklzxcvbnñm";
-  char *uper = "QWERTYUIOPASDFGHJKLZXCVBNÑM";
-  char *symb = "!@#$%^&*()_+~`,<>./|][{}':;*-=+";
-  int   nums = "1234567890";
-  return 0;
-}
